895fdf125c37f5250a6b1204b59800ab5bfc0496
ATHERLY-Evan-24012805 <evan.atherly@etu.univ-amu.fr>
2026-04-10 18:53:14 +0200

    changement dans le style

--- js/pages/DocumentationPage.js
+++ js/pages/DocumentationPage.js
@@ -1,37 +1,31 @@
 const DocumentationPage = {
   render() {
     return `
-<nav >
-      <ul>
-        <li><a href="index.html">Accueil</a></li>
-        <li><a href="documentation.html">Documentation</a></li>
-        <li><a href="">Information du compte</a></li>
-        <li><a href="">Déconnexion</a></li>
-      </ul>
-    </nav>
+    <div id="page-doc">
+        <div class="container">
+            <h1>Documentation du projet HotHotHot</h1>
+            
+            <h2>Présentation de l'équipe :</h2>
+            <ul>
+                <li><strong>Acemyan--de Oliveira</strong> : Développeur</li>
+                <li><strong>Atherly Evan</strong> : Développeur</li>
+                <li><strong>Nahi Amine</strong> : Développeur</li>
+                <li><strong>Palot Thomas</strong> : Développeur</li>
+            </ul>
 
-    <div class="container">
-        <h1>Documentation du projet HotHotHot</h1>
-        <h2>Présentation de l'équipe :</h2>
-        <ul>
-            <li><strong>Acemyan--de Oliveira</strong> : Développeur</li>
-            <li><strong>Atherly Evan</strong> : Développeur</li>
-            <li><strong>Nahi Amine</strong> : Développeur</li>
-            <li><strong>Palot Thomas</strong> : Développeur</li>
-        </ul>
-
-        <h2>Outils utilisés et organisation :</h2>
-        <ul>
-            <li>
-                <strong>Dépôt git :</strong> 
-                <a href="https://github.com/AmineNahi/Complement-Web" target="_blank" rel="noopener noreferrer">
-                    Lien vers le dépôt GitHub
-                </a>
-            </li>
-            <li><strong>Gestion de projet :</strong> </li>
-            <li><strong>Principales difficultés :</strong> </li>
-            <li><strong>Changements si le projet devait être recommencé :</strong> </li>
-        </ul>
+            <h2>Outils utilisés et organisation :</h2>
+            <ul>
+                <li>
+                    <strong>Dépôt git :</strong> 
+                    <a href="https://github.com/AmineNahi/Complement-Web" target="_blank" rel="noopener noreferrer">
+                        Lien vers le dépôt GitHub
+                    </a>
+                </li>
+                <li><strong>Gestion de projet :</strong> </li>
+                <li><strong>Principales difficultés :</strong> </li>
+                <li><strong>Changements si le projet devait être recommencé :</strong> </li>
+            </ul>
+        </div>
     </div>
     `;
   }
